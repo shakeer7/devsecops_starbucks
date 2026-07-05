b4cdb5cef575b1d9282eb4780ec6533628cdeb80
Shakeer Mohammed <mds.shakeer@gmail.com>
2026-07-05 16:13:57 +0530

    devsecops pipeline11

--- src/App.js
+++ src/App.js
@@ -1,39 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import {
   BrowserRouter,
   Routes,
-  Route, Navigate
+  Route,
+  Navigate,
 } from "react-router-dom";
-import Header from './components/Header';
-import Gift from './pages/Gift';
-import Order from './pages/Order';
-import Pay from './pages/Pay';
-import Store from './pages/Store';
-import Home from './pages/Home';
-import Search  from './pages/Search';
-import Footer from './components/Footer';
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import Gift from "./pages/Gift";
+import Order from "./pages/Order";
+import Pay from "./pages/Pay";
+import Store from "./pages/Store";
+import Search from "./pages/Search";
 
 function App() {
   return (
-    
     <BrowserRouter>
-          <Header/>
-      <Routes>
+      <div className="min-h-screen flex flex-col bg-gray-50">
+        <Header />
 
-        <Route exact path="/" element={<Navigate replace to="/dashboard" />} />
-        <Route  path="/dashboard" element={<Home />} />
-        <Route path='/giftcards' element={<Gift />} />
-        <Route path='/ordering' element={<Order />} />
-        <Route path='/pay' element={<Pay />} />
-        <Route path='/store' element={<Store />} />
-        <Route path='/search' element={<Search />} />
+        <main className="flex-grow pt-20 lg:pt-24">
+          <Routes>
+            <Route
+              path="/"
+              element={<Navigate replace to="/dashboard" />}
+            />
 
+            <Route path="/dashboard" element={<Home />} />
+            <Route path="/giftcards" element={<Gift />} />
+            <Route path="/ordering" element={<Order />} />
+            <Route path="/pay" element={<Pay />} />
+            <Route path="/store" element={<Store />} />
+            <Route path="/search" element={<Search />} />
+          </Routes>
+        </main>
 
-      </Routes>
-      <Footer/>
+        <Footer />
+      </div>
     </BrowserRouter>
-
   );
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,73 +1,195 @@
-import React from 'react'
-import { BsInstagram, BsTwitter } from 'react-icons/bs'
-import { CgFacebook } from "react-icons/cg"
-import { Link } from 'react-router-dom'
-import logo from "../assests/img/logo.png"
-import appstoreiOSImage from "../assests/img/appstoreiOS.png"
-import appstoreAndroidImage from "../assests/img/appstoreAndroid.png"
+import React from "react";
+import { BsInstagram, BsTwitter } from "react-icons/bs";
+import { CgFacebook } from "react-icons/cg";
+import { Link } from "react-router-dom";
+
+import logo from "../assests/img/logo.png";
+import appstoreiOSImage from "../assests/img/appstoreiOS.png";
+import appstoreAndroidImage from "../assests/img/appstoreAndroid.png";
 
 const Footer = () => {
-    return (
-        <div className='bg-[#0e382c] w-full py-5 hidden sm:block'>
-            <div className='p-8 px-4 sm:px-10 md:px-20 lg:pl-24  lg:pr-28 relative mt-3 '>
-                <div className='flex flex-wrap justify-between'>
-                    <div className='min-w-[50px]'>
-                        <img src={logo} className="w-16" />
-                    </div>
-                    <div className='flex flex-col text-white'>
-                        <Link to={""} className="font-s_bold text-lg mb-4">AboutUS</Link>
-                        <Link to={""} className="text-sm mb-6">Our Heritage</Link>
-                        <Link to={""} className="text-sm mb-6">Our Company</Link>
-                        <Link to={""} className="text-sm mb-6">Coffee house</Link>
-                    </div>
-                    <div className='flex flex-col text-white'>
-                        <Link to={""} className="font-s_bold text-lg mb-4">Responsibility</Link>
-                        <Link to={""} className="text-sm mb-6">Community</Link>
-                        <Link to={""} className="text-sm mb-6">Ethical Sourcing</Link>
-                        <Link to={""} className="text-sm mb-6">Enviroment</Link>
-                        <Link to={""} className="text-sm mb-6">Diversity</Link>
-                    </div>
-                    <div className='flex flex-col text-white'>
-                        <Link to={""} className="font-s_bold text-lg mb-4">Quick Links</Link>
-                        <Link to={""} className="text-sm mb-6">Delivery</Link>
-                        <Link to={""} className="text-sm mb-6">Season's Gifting</Link>
-                        <Link to={""} className="text-sm mb-6">Carees</Link>
-                        <Link to={""} className="text-sm mb-6">Customer Service</Link>
-                        <Link to={""} className="text-sm mb-6">FAQs</Link>
-                        
-                        
-                    </div>
-                    <div className='flex flex-col text-white'>
-                        <Link to={""} className="font-s_bold text-lg mb-4">SOCIAL MEDIA</Link>
-                        <div className='flex gap-5 text-2xl'>
-                            <a href='https://www.instagram.com/starbucksindia/'><BsInstagram /></a>
-                            <a href='https://www.facebook.com/starbucksindia'><CgFacebook /></a>
-                            <a href='https://twitter.com/starbucksindia'><BsTwitter /></a>
-                        </div>
-                    </div>
-                    <div className='flex flex-col gap-5'>
-                        <a href='https://apps.apple.com/in/app/starbucks-india/id1210203958'>
-                            <img src={appstoreiOSImage} />
-                        </a>
-                        <a href='https://play.google.com/store/apps/details?id=com.starbucks.in'>
-                            <img src={appstoreAndroidImage} />
-                        </a>
-                    </div>
-                </div>
+  return (
+    <footer className="hidden sm:block bg-gray-900 text-gray-300 mt-20">
+      <div className="max-w-7xl mx-auto px-8 lg:px-16 py-14">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-10">
 
-                <div className='border-t border-solid mt-7 text-gray-100 py-3 flex justify-between'>
-                        <div className='text-sm'>
-                            <Link to={""} className="pr-5 border-r">Web Accessiblity</Link>
-                            <Link to={""} className="pr-3 pl-3 border-r">Privacy Statement</Link>
-                            <Link to={""} className="pr-3 pl-3 border-r">Terms of Use</Link>
-                            <Link to={""} className="pr-3 pl-3">Contact Us</Link>
-                        </div>
-                        <p className='text-xs'>© 2023 Starbucks Coffee Company. All rights reserved.</p>
-                </div>
+          {/* Logo */}
+          <div>
+            <img
+              src={logo}
+              alt="Starbucks Logo"
+              className="w-16 hover:scale-105 transition duration-300"
+            />
+          </div>
+
+          {/* About */}
+          <div>
+            <h3 className="text-white font-semibold mb-5 text-lg">
+              About Us
+            </h3>
+
+            <div className="flex flex-col gap-3 text-sm">
+              <Link to="" className="hover:text-green-400 transition">
+                Our Heritage
+              </Link>
+
+              <Link to="" className="hover:text-green-400 transition">
+                Our Company
+              </Link>
+
+              <Link to="" className="hover:text-green-400 transition">
+                Coffeehouse
+              </Link>
             </div>
+          </div>
 
+          {/* Responsibility */}
+          <div>
+            <h3 className="text-white font-semibold mb-5 text-lg">
+              Responsibility
+            </h3>
+
+            <div className="flex flex-col gap-3 text-sm">
+              <Link to="" className="hover:text-green-400 transition">
+                Community
+              </Link>
+
+              <Link to="" className="hover:text-green-400 transition">
+                Ethical Sourcing
+              </Link>
+
+              <Link to="" className="hover:text-green-400 transition">
+                Environment
+              </Link>
+
+              <Link to="" className="hover:text-green-400 transition">
+                Diversity
+              </Link>
+            </div>
+          </div>
+
+          {/* Quick Links */}
+          <div>
+            <h3 className="text-white font-semibold mb-5 text-lg">
+              Quick Links
+            </h3>
+
+            <div className="flex flex-col gap-3 text-sm">
+              <Link to="" className="hover:text-green-400 transition">
+                Delivery
+              </Link>
+
+              <Link to="" className="hover:text-green-400 transition">
+                Seasonal Gifting
+              </Link>
+
+              <Link to="" className="hover:text-green-400 transition">
+                Careers
+              </Link>
+
+              <Link to="" className="hover:text-green-400 transition">
+                Customer Service
+              </Link>
+
+              <Link to="" className="hover:text-green-400 transition">
+                FAQs
+              </Link>
+            </div>
+          </div>
+
+          {/* Social */}
+          <div>
+            <h3 className="text-white font-semibold mb-5 text-lg">
+              Follow Us
+            </h3>
+
+            <div className="flex gap-5 text-2xl">
+              <a
+                href="https://www.instagram.com/starbucksindia/"
+                target="_blank"
+                rel="noreferrer"
+                className="hover:text-green-400 hover:scale-125 transition"
+              >
+                <BsInstagram />
+              </a>
+
+              <a
+                href="https://www.facebook.com/starbucksindia"
+                target="_blank"
+                rel="noreferrer"
+                className="hover:text-green-400 hover:scale-125 transition"
+              >
+                <CgFacebook />
+              </a>
+
+              <a
+                href="https://twitter.com/starbucksindia"
+                target="_blank"
+                rel="noreferrer"
+                className="hover:text-green-400 hover:scale-125 transition"
+              >
+                <BsTwitter />
+              </a>
+            </div>
+          </div>
+
+          {/* Store Buttons */}
+          <div className="flex flex-col gap-4">
+            <a
+              href="https://apps.apple.com/in/app/starbucks-india/id1210203958"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={appstoreiOSImage}
+                alt="Download on App Store"
+                className="hover:scale-105 transition duration-300"
+              />
+            </a>
+
+            <a
+              href="https://play.google.com/store/apps/details?id=com.starbucks.in"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={appstoreAndroidImage}
+                alt="Get it on Google Play"
+                className="hover:scale-105 transition duration-300"
+              />
+            </a>
+          </div>
         </div>
-    )
-}
 
-export default Footer
+        {/* Bottom Section */}
+
+        <div className="border-t border-gray-700 mt-10 pt-6 flex flex-col lg:flex-row justify-between items-center gap-4">
+
+          <div className="flex flex-wrap justify-center gap-4 text-sm">
+            <Link to="" className="hover:text-green-400 transition">
+              Web Accessibility
+            </Link>
+
+            <Link to="" className="hover:text-green-400 transition">
+              Privacy Statement
+            </Link>
+
+            <Link to="" className="hover:text-green-400 transition">
+              Terms of Use
+            </Link>
+
+            <Link to="" className="hover:text-green-400 transition">
+              Contact Us
+            </Link>
+          </div>
+
+          <p className="text-xs text-gray-500 text-center">
+            © 2026 Starbucks Coffee Company. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
